001c36aefb17d1d9767de88bdc26815ca2ae53ff
shikelong <578622705@qq.com>
2019-03-15 13:18:56 +0900

    create start up templete.

---
+++ android/app/src/main/java/com/example/flutter_teamcity/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.flutter_teamcity;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}

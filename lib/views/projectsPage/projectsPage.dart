bcd1844328164c90a4ec9fd3fdcda1d0c0857694
shikelong <578622705@qq.com>
2019-03-22 18:57:41 +0900

    fix some issue.

--- lib/views/projectsPage/projectsPage.dart
+++ lib/views/projectsPage/projectsPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 import 'dart:convert';
 import '../../model/project.dart';
 import '../../utils/http.dart';
@@ -14,11 +15,13 @@ Future<List<Project>> fetchProjects() async {
     projects.forEach((item) {
       result.add(Project.fromJson(item));
     });
-
   } else {
     // If that call was not successful, throw an error.
     //throw Exception('Failed to load projects');
   }
+
+  print("result is ${result.toString()}");
+
   return result;
 }
 
@@ -43,19 +46,19 @@ class _ProjectsPageState extends State<ProjectsPage> {
         future: fetchProjects(),
         builder: (context, snap) {
           if (snap.hasData && snap.data.length > 0) {
-//            List<ListTile> content = new List<ListTile>();
-//            snap.data.forEach((item) {
-//              content.add(ListTile(
-//                key: Key(item.id),
-//                title: Text(item.name),
-//                subtitle: Text(item.description),
-//                leading: Icon(Icons.alarm_on),
-//              ));
-//            });
-            // return Column(
-            //   children: content,
-            // );
-            return Text('sadsda');
+             return ListView.builder(
+                 itemBuilder: (BuildContext context, int index){
+                   var item = snap.data[index];
+                   return ListTile(
+                     key: Key(item.id),
+                     title: Text(item.name),
+                     subtitle: Text(item.description),
+                     leading: Icon(Icons.alarm_on),
+                   );
+                 },
+                  itemCount: snap.data.length,
+             );
+            //return Text('sadsda');
           } else if (snap.hasError) {
             return Text(snap.error);
           }
